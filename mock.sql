da8b1bc8ba6d57ca20c2c3fb64d2520de4f3b286
david <davidhunt@mitre.org>
2019-09-17 12:32:03 -0400

    removing unused table

--- mock.sql
+++ mock.sql
@@ -1,2 +1,1 @@
 CREATE TABLE if not exists sim_response (id integer primary key AUTOINCREMENT, ability_id text, paw text, status integer, response text, UNIQUE (ability_id, paw) ON CONFLICT IGNORE);
-CREATE TABLE if not exists sim_response_map (sim_response_id integer, property text, value text, UNIQUE (sim_response_id, property) ON CONFLICT IGNORE);
