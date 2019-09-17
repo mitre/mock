881bd0cb1872575144fbd67fb7674e9ffb93482a
david <davidhunt@mitre.org>
2019-09-17 06:57:57 -0400

    adding refinement

--- hook.py
+++ hook.py
@@ -1,9 +1,10 @@
 import asyncio
+from random import randint
 
 from plugins.mock.app.mock_svc import MockService
 
 name = 'Mock'
-description = 'A virtual simulation environment for running operations'
+description = 'Simulated agents for testing operations with no external dependencies'
 address = None
 
 
@@ -12,7 +13,10 @@ async def initialize(app, services):
     simulation_svc = MockService(services)
     simulated_responses = [r for r in services.get('agent_svc').strip_yml('plugins/%s/conf/responses.yml' % name.lower())[0]]
     await simulation_svc.load_simulation_results(simulated_responses)
-    agents = [a for a in services.get('agent_svc').strip_yml('plugins/%s/conf/config.yml' % name.lower())[0]]
+    agents = [a for a in services.get('agent_svc').strip_yml('plugins/%s/conf/config.yml' % name.lower())[0] if a['enabled']]
     loop = asyncio.get_event_loop()
     for a in agents:
+        a['pid'], a['ppid'], a['sleep'] = randint(1000,10000), randint(1000, 10000), randint(55, 65)
+        a['architecture'] = None
+        a['server'] = 'http://localhost:8888'
         loop.create_task(simulation_svc.run(a))
