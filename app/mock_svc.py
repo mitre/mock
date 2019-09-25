042a79e091705158c7707567e6a5644abaa5a248
clittle <clittle@mitre.org>
2019-09-25 16:28:03 -0400

    Saving work for the day - still need to implement actual spawn

--- app/mock_svc.py
+++ app/mock_svc.py
@@ -1,6 +1,6 @@
 import asyncio
 import json
-
+from random import randint
 
 class MockService:
 
@@ -29,6 +29,9 @@ class MockService:
                                 response=self.agent_svc.encode_string(p['response']))
                 await self.data_svc.create('sim_response', response)
 
+    async def set_agent_listing(self, agents):
+        self.agents = agents
+
     """ PRIVATE """
 
     async def _get_simulated_response(self, link_id, paw):
@@ -37,5 +40,15 @@ class MockService:
         sim_response = await self.data_svc.get('sim_response', dict(ability_id=ability['ability_id'], paw=paw))
         if not sim_response:
             return '', 0
+        if "|SPAWN|" in sim_response[0]['response']:
+            await self._spawn_new_sim(link)
+            return 'spawned new agent', sim_response[0]['status']
         return sim_response[0]['response'], sim_response[0]['status']
 
+    async def _spawn_new_sim(self, link):
+        a = {} # need to find target and get information from self.agents
+        a['pid'], a['ppid'], a['sleep'] = randint(1000,10000), randint(1000, 10000), randint(55, 65)
+        a['architecture'] = None
+        a['server'] = 'http://localhost:8888'
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.run(a))
--- hook.py
+++ hook.py
@@ -13,7 +13,9 @@ async def initialize(app, services):
     simulation_svc = MockService(services)
     simulated_responses = [r for r in services.get('agent_svc').strip_yml('plugins/%s/conf/responses.yml' % name.lower())[0]]
     await simulation_svc.load_simulation_results(simulated_responses)
-    agents = [a for a in services.get('agent_svc').strip_yml('plugins/%s/conf/config.yml' % name.lower())[0]]
+    all_agents = [a for a in services.get('agent_svc').strip_yml('plugins/%s/conf/config.yml' % name.lower())[0]]
+    await simulation_svc.set_agent_listing(all_agents)
+    agents = [a for a in all_agents if 'expansion' not in a]
     loop = asyncio.get_event_loop()
     for a in agents:
         a['pid'], a['ppid'], a['sleep'] = randint(1000,10000), randint(1000, 10000), randint(55, 65)
