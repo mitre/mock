08e3ff0741ca27c94c8a3f7a456ad09df4e21f23
Hunt, David <davidhunt@mitre.org>
2019-11-21 13:14:01 -0500

    removing enable flag. not needed

--- hook.py
+++ hook.py
@@ -7,7 +7,6 @@ from plugins.mock.app.simulation_svc import SimulationService
 name = 'Mock'
 description = 'Simulated scenarios for testing operations with no external dependencies'
 address = '/plugin/mock/gui'
-enabled = False
 
 
 async def enable(app, services):
