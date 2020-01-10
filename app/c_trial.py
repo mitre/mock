70de3e232c0ffddfa33dc845fde19a194cf1514d
Hunt, David <davidhunt@mitre.org>
2020-01-10 15:21:07 -0500

    adding the concept of trials, which allow you to run a bulk set of simulated operations

--- app/c_trial.py
+++ app/c_trial.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+
+from base64 import b64encode
 from datetime import datetime
+from io import BytesIO
 
 from app.utility.base_object import BaseObject
 
@@ -12,7 +16,7 @@ class Trial(BaseObject):
     @property
     def display(self):
         return dict(name=self.name, number=self.number, start=self.start.strftime('%Y-%m-%d %H:%M:%S'),
-                    operations=[o.display for o in self.operations])
+                    operations=[o.display for o in self.operations], charts=self._calculate_charts())
 
     def __init__(self, name, number):
         self.name = name
@@ -25,3 +29,16 @@ class Trial(BaseObject):
         if not existing:
             ram['trials'].append(self)
             return self.retrieve(ram['trials'], self.unique)
+
+    """ PRIVATE """
+
+    def _calculate_charts(self):
+        for op in self.operations:
+
+        plt.plot([1, 2, 3, 4, 8, 12, 20])
+        plt.xlabel('some numbers')
+        plt.ylabel('some numbers')
+        temp_file = BytesIO()
+        plt.savefig(temp_file, format='png')
+        encoded = b64encode(temp_file.getvalue()).decode('utf-8')
+        return ['<img src=\'data:image/png;base64,{}\'>'.format(encoded)]
