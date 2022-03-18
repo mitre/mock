2facad9b6fbd1ba69587fa44c95658f2247df9f2
Kyle Heaton <heatonk@purdue.edu>
2022-03-18 10:42:21 -0400

    Added None parser check

--- app/result_generator.py
+++ app/result_generator.py
@@ -14,6 +14,8 @@ class ResultGenerator(BaseWorld):
         }
 
     async def generate(self, words):
+        if self.parser is None:
+            return
         for parser in self.parser:
             for cfg in parser.parserconfigs:
                 for _ in range(random.randint(1, 5)):
