0a09e457a465816507e608506e93e79a71814092
Adam Gaudreau <argaudreau@gmail.com>
2022-12-14 11:23:18 -0500

    Add Vue UI for magma

---
+++ gui/views/mock.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template lang="pug">
+.content    
+    h2 Mock
+    p Simulated agents
+hr
+
+.content
+    p 
+        | Mock adds a set of simulated agents to CALDERA and allows you to run complete operations without hooking any
+        | other computers up to your server. All simulated agents will appear as normal agents in the UI and can be
+        | used in any operation.
+
+.is-flex.is-align-items-center.is-justify-content-center
+    a.button.is-primary(href="/docs/Plugin-library.html#mock" target="_blank")
+        span Read more here
+        span.icon
+            font-awesome-icon(icon="fas fa-angle-right")
+
+</template>
--- hook.py
+++ hook.py
@@ -1,5 +1,4 @@
 from app.utility.base_world import BaseWorld
-from plugins.mock.app.mock_gui_api import MockGuiApi
 from plugins.mock.app.simulation_svc import SimulationService
 
 name = 'Mock'
@@ -12,8 +11,6 @@ async def enable(services):
     app = services.get('app_svc').application
     all_agents = [a for a in services.get('data_svc').strip_yml('plugins/mock/conf/agents.yml')[0]]
     simulation_svc = SimulationService(services, all_agents)
-    mock_gui_api = MockGuiApi(services=services)
-    app.router.add_route('GET', '/plugin/mock/gui', mock_gui_api.splash)
     agents = [a for a in all_agents if a['enabled']]
     for a in agents:
         await simulation_svc.start_agent(a)
